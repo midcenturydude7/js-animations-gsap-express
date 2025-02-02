471dae7dd8b6a3bb9e0566db3af5ff9275039c16
midCenturyDude7 <mgriffes@gmail.com>
2025-02-01 22:42:33 -0800

    Working with different easing properties

--- js/basic-tween.js
+++ js/basic-tween.js
@@ -1,5 +1,8 @@
-gsap.fromTo(
-  ".fred",
-  { x: 700, y: 400, opacity: 0 },
-  { x: 400, y: 200, opacity: 1, scale: 3, duration: 3 }
-);
+// gsap.fromTo(
+//   ".fred",
+//   { x: 700, y: 400, opacity: 0 },
+//   { x: 400, y: 200, opacity: 1, scale: 3, duration: 3 }
+// );
+
+gsap.to(".fred1", { duration: 3, x: 600, ease: "back" });
+gsap.to(".fred2", { duration: 3, x: 600, ease: "linear" });
