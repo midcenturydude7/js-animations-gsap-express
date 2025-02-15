b3a79aeaa88f782b5679151b37aaa96ceff2c183
midCenturyDude7 <mgriffes@gmail.com>
2025-02-14 20:54:13 -0800

    Working with tween controls and DOM manipulation

--- js/tweenControl.js
+++ js/tweenControl.js
@@ -1,1 +1,11 @@
-gsap.to(".green", { duration: 3, x: 600, ease: "linear" });
+let tween = gsap.to(".green", {
+  duration: 3,
+  x: 600,
+  ease: "linear",
+  paused: true,
+});
+
+document.querySelector("#play").onclick = () => tween.play();
+document.querySelector("#pause").onclick = () => tween.pause();
+document.querySelector("#reverse").onclick = () => tween.reverse();
+document.querySelector("#restart").onclick = () => tween.restart();
