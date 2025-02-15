113e5f36a18d4c785fa6e35e4305ebde3c2e89f5
midCenturyDude7 <mgriffes@gmail.com>
2025-02-15 15:18:50 -0800

    Setting up timeline and control labels

--- js/timelines.js
+++ js/timelines.js
@@ -1,12 +1,23 @@
-gsap.set("#demo", { scale: 0.7 });
+document.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    event.target.focus();
+  }
+});
 
-gsap
+const animation = gsap
   .timeline()
   .from("#demo", { duration: 1, opacity: 0 })
   .from("#title", { opacity: 0, scale: 0, ease: "back" })
   .from(
     "#freds img",
     { y: 160, stagger: 0.25, duration: 0.8, ease: "back" },
-    "+=2"
+    "+=0.5"
   )
-  .from("#time", { xPercent: 100, duration: 0.8 }, 1);
+  .add("#test")
+  .from("#time", { xPercent: 100, duration: 1 });
+
+document.getElementById("play").onclick = () => animation.play();
+document.getElementById("pause").onclick = () => animation.pause();
+document.getElementById("reverse").onclick = () => animation.reverse();
+document.getElementById("restart").onclick = () => animation.restart();
+document.getElementById("test").onclick = () => animation.play("test");
