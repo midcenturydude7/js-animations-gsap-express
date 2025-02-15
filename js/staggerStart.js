f2160b59fd65cdb3540226422abd2c06afb42a2c
midCenturyDude7 <mgriffes@gmail.com>
2025-02-14 20:21:19 -0800

    Working with stagger properties

--- js/staggerStart.js
+++ js/staggerStart.js
@@ -1,1 +1,10 @@
-gsap.to("#freds img", { y: -50 });
+gsap.to("#freds img", {
+  y: -50,
+  stagger: {
+    each: 0.2,
+    from: "end",
+  },
+  onComplete: () => {
+    console.log("Animation completed!");
+  },
+});
