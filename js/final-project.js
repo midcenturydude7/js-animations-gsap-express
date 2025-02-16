8a0de39c38bf9dbe47a8b63714e10ffef7cdc8fe
midCenturyDude7 <mgriffes@gmail.com>
2025-02-16 14:38:16 -0800

    Removed auto flashing bug with autoAlpha

--- js/final-project.js
+++ js/final-project.js
@@ -1,15 +1,20 @@
 const tl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
-tl.from("#demo", { ease: "linear" })
-  .from("h1", { x: 80, duration: 1 })
-  .from("h2", { x: -80, duration: 1 }, "<")
-  .from("p", { y: 30 }, "-=0.2")
-  .from("button", { y: 50 }, "-=0.4")
-  .from(
-    "#items > g",
-    {
-      scale: 0,
-      transformOrigin: "50% 50%",
-      stagger: 0.1,
-    },
-    "-=0.5"
-  );
+
+const init = () => {
+  tl.from("#demo", { ease: "linear", autoAlpha: 0, delay: 0.8 })
+    .from("h1", { x: 80, duration: 1 })
+    .from("h2", { x: -80, duration: 1 }, "<")
+    .from("p", { y: 30 }, "-=0.2")
+    .from("button", { y: 50 }, "-=0.4")
+    .from(
+      "#items > g",
+      {
+        scale: 0,
+        transformOrigin: "50% 50%",
+        stagger: 0.1,
+      },
+      "-=0.5"
+    );
+};
+
+window.addEventListener("load", () => init());
