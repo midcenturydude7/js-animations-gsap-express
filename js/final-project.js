6b30364172e2534715309c22675ca77e4ae008d4
midCenturyDude7 <mgriffes@gmail.com>
2025-02-16 14:13:03 -0800

    Tweaking timing properties

--- js/final-project.js
+++ js/final-project.js
@@ -1,11 +1,15 @@
 const tl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
 tl.from("#demo", { ease: "linear" })
-  .from("h1", { x: 80 })
-  .from("h2", { x: -80 })
-  .from("p", { y: 30 })
-  .from("button", { y: 30 })
-  .from("#items > g", {
-    scale: 0,
-    stagger: 0.1,
-    transformOrigin: "50% 50%",
-  });
+  .from("h1", { x: 80, duration: 1 })
+  .from("h2", { x: -80, duration: 1 }, "<")
+  .from("p", { y: 30 }, "-=0.2")
+  .from("button", { y: 50 }, "-=0.4")
+  .from(
+    "#items > g",
+    {
+      scale: 0,
+      transformOrigin: "50% 50%",
+      stagger: 0.1,
+    },
+    "-=0.5"
+  );
