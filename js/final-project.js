6871cac55bb6abe48e90ff39dd084e042652a116
midCenturyDude7 <mgriffes@gmail.com>
2025-02-16 12:55:29 -0800

    Working with timeline defaults to simply code and smooth out animations

--- js/final-project.js
+++ js/final-project.js
@@ -1,12 +1,11 @@
-const tl = gsap.timeline();
-tl.from("#demo", { opacity: 0 })
-  .from("h1", { x: 80, opacity: 0 })
-  .from("h2", { x: -80, opacity: 0 })
-  .from("p", { y: 30, opacity: 0 })
-  .from("button", { y: 30, opacity: 0 })
+const tl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
+tl.from("#demo", { ease: "linear" })
+  .from("h1", { x: 80 })
+  .from("h2", { x: -80 })
+  .from("p", { y: 30 })
+  .from("button", { y: 30 })
   .from("#items > g", {
     scale: 0,
     stagger: 0.1,
-    opacity: 0.1,
     transformOrigin: "50% 50%",
   });
