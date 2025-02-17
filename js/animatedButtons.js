6fa5acaeb567d4c41818f85bd49a602122b4b62c
midCenturyDude7 <mgriffes@gmail.com>
2025-02-17 13:04:17 -0800

    Completed basic animated buttons lesson

--- js/animatedButtons.js
+++ js/animatedButtons.js
@@ -1,1 +1,25 @@
-const item = document.querySelector(".item");
+const items = document.querySelectorAll(".item");
+
+gsap.from(".wrapper", { autoAlpha: 0, duration: 1, delay: 2 });
+
+items.forEach((item, index) => {
+  // console.log(item, index);
+  const tl = gsap
+    .timeline({ paused: true })
+    .to(item.querySelector(".text"), {
+      color: "white",
+      scale: 1.3,
+      transformOrigin: "left center",
+      opacity: 1,
+      x: 10,
+    })
+    .to(item.querySelector(".dot"), { backgroundColor: "#F93", scale: 1.5 }, 0);
+
+  item.addEventListener("mouseenter", () => {
+    tl.play();
+  });
+
+  item.addEventListener("mouseleave", () => {
+    tl.reverse();
+  });
+});
