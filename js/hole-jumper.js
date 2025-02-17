fe6e02104c6ce68dd366c6467f99418a7c8a8ac6
midCenturyDude7 <mgriffes@gmail.com>
2025-02-16 21:49:10 -0800

    Finished animation

--- js/hole-jumper.js
+++ js/hole-jumper.js
@@ -1,11 +1,11 @@
 const hole = document.querySelector(".hole");
 const herman = document.querySelector(".herman");
 const shadow = document.querySelector(".shadow");
-const tl = gsap.timeline();
+const tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 0.5 });
 
 tl.from(hole, { scale: 0, repeat: 1, yoyo: true })
   .fromTo(herman, { y: 160, scaleY: 2 }, { y: -175, scaleY: 1 }, 0.2)
-  .to(herman, { y: 0, ease: "power1.in" }, ">")
+  .to(herman, { y: -4, ease: "power1.in" }, ">")
   .to(herman, {
     scaleY: 0.5,
     scaleX: 1.3,
